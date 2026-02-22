f3b4cc97cad74b967424f2269b720de39e4e85bd
JDillard026 <jacobdillard51@yahoo.com>
2026-02-22 17:37:09 -0500

    Delete RootLayout component and metadata
    Remove RootLayout component and associated metadata.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,1 @@
-export const metadata = { title: "Life Command Center" };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: "Arial", padding: 24 }}>
-        {children}
-      </body>
-    </html>
-  );
-}
